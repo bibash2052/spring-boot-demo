9e6913ed4cbcaa72e0e237254f0c7859cc41e0fb
Bibash <bibash2052@gmail.com>
2022-02-23 13:10:59 +0545

    initail commit

---
+++ src/main/java/com/example/demo/domain/User/User.java
@@ -0,0 +1,26 @@
+package com.example.demo.domain.User;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+}
---
+++ src/main/java/com/example/demo/domain/User/UserService.java
@@ -0,0 +1,4 @@
+package com.example.demo.domain.User;
+
+public interface UserService {
+}
